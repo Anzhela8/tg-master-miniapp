243fed35faa327d9867a2b48e5a0b91f372bf315
Анжела <damn.angel.666@gmail.com>
2025-03-28 00:01:01 +0300

    Initial commit for Vercel

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+import os
 
 app = Flask(__name__, static_folder='static', static_url_path='/static')
 
@@ -13,4 +14,5 @@ def contact():
     return jsonify({'message': f'Привет, {user_name}! Спасибо за интерес к TG Мастер! Скоро с вами свяжутся.'})
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    port = int(os.getenv("PORT", 5000))  # Используем PORT от Vercel или 5000 локально
+    app.run(host='0.0.0.0', port=port, debug=True)
